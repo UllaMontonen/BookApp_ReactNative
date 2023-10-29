9ac135704a0c0b1e9e9e5c3da49d6f4040d6683e
UllaMontonen <u.montonen@gmail.com>
2023-10-29 16:32:39 +0200

    Home page done

--- App.js
+++ App.js
@@ -6,34 +6,39 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from './Home'
 import Search from './Search';
 import ReadingList from './ReadingList';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, push, ref, onValue } from 'firebase/database';
+
+
+// Firebase instal komento tehty
 
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-      return (
-        <NavigationContainer>
-          <Tab.Navigator
-          screenOptions={({ route }) => ({  // Customizing navigator using screenOptions
-            tabBarIcon: ({ focused, color, size }) => {
-          // Function tabBarIcon is given the focused state,
-          // color and size params
-          let iconName;
-          if (route.name === 'Home') {
-            iconName = 'md-home';
-          } else if (route.name === 'Search') {
-            iconName = 'md-search';
-          } else if (route.name === 'ReadingList') {
-            iconName = 'md-book';
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-          >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Search" component={Search} />
-            <Tab.Screen name="ReadingList" component={ReadingList} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      );
-    }
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({  // Customizing navigator using screenOptions
+          tabBarIcon: ({ focused, color, size }) => {
+            // Function tabBarIcon is given the focused state,
+            // color and size params
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'md-home';
+            } else if (route.name === 'Search') {
+              iconName = 'md-search';
+            } else if (route.name === 'ReadingList') {
+              iconName = 'md-book';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="ReadingList" component={ReadingList} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- Home.js
+++ Home.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Button } from '@rneui/themed';
+import { Header } from '@rneui/themed';
+import { StatusBar } from 'expo-status-bar';
 
 
 
 
 export default function Home({ navigation }) {
     return (
+
         <View style={styles.container}>
-            <Button
-        title="Find Books"
-        onPress={() => navigation.navigate('Search')} // Navigate to Search screen
-      />
-      <Button
-        title="My Reading List"
-        onPress={() => navigation.navigate('ReadingList')} // Navigate to ReadingList screen
-      />
+            <View style={styles.header}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Welcome to the Reading Book App</Text>
+            </View>
+            <Image
+                style={styles.imgae}
+                source={require('./pictures/owl.webp')} />
+            <View style={styles.button}>
+                <Button
+                    title="Find Books"
+                    onPress={() => navigation.navigate('Search')} // Navigate to Search screen
+                />
+            </View>
+            <View style={styles.button}>
+                <Button
+                    title="My Reading List"
+                    onPress={() => navigation.navigate('ReadingList')} // Navigate to ReadingList screen
+                />
+            </View>
+            <StatusBar style='auto' />
         </View>
     )
 }
@@ -24,4 +39,17 @@ const styles = StyleSheet.create({
         marginTop: 10,
         alignItems: 'center',
     },
+    header: {
+        marginBottom: 50,
+        marginTop: 30,
+    },
+    button: {
+        marginBottom: 10,
+        width: 270, 
+    },
+    imgae: {
+        width: 270, 
+        height: 260,
+        marginBottom: 40
+    }
 });
