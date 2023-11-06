a62e738a6bdf88af1e23a627fc17d2630ad22cb6
UllaMontonen <u.montonen@gmail.com>
2023-11-06 15:00:38 +0200

    Fixed the size of the thumbnail picture

--- SearchScreen.js
+++ SearchScreen.js
@@ -16,7 +16,7 @@ export default function SearchScreen() {
             const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
-            } 
+            }
             const data = await response.json();
             setResults(data.items);
         } catch (error) {
@@ -33,29 +33,29 @@ export default function SearchScreen() {
     //    "thumbnail": "http://books.google.com/books/content?id=ac6OEAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
     //  },
 
-   
-
-// <ListItem.Subtitle>{item.volumeInfo.description}</ListItem.Subtitle>
 
 
-// HOX!!! KOKEILE SMALL THUMBNAIL VAIHTOEHTOA!!
+    // <ListItem.Subtitle>{item.volumeInfo.description}</ListItem.Subtitle>
+
+
+    // HOX!!! KOKEILE SMALL THUMBNAIL VAIHTOEHTOA!!
 
     // Rendering books for flatlist
     const renderItem = ({ item }) => (
         <ListItem bottomDivider>
             <ListItem.Content>
                 <View style={styles.row}>
-                <Image
-        style={{ width: 100, height: 200, flex: 1 }}
-        source={{
-            uri: `${item.volumeInfo.imageLinks.thumbnail}`,
-        }}
-    />
-    <View style={styles.info}>
-                    <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
-                    <ListItem.Subtitle>Author/s: {item.volumeInfo.authors}</ListItem.Subtitle>
-                    <ListItem.Subtitle>Published year: {item.volumeInfo.publishedDate}</ListItem.Subtitle>
-                    <ListItem.Subtitle>Pages: {item.volumeInfo.pageCount}</ListItem.Subtitle>
+                    <Image
+                        style={{ width: 100, height: 150, flex: 1 }}
+                        source={{
+                            uri: `${item.volumeInfo.imageLinks.thumbnail}`,
+                        }}
+                    />
+                    <View style={styles.info}>
+                        <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
+                        <ListItem.Subtitle>Author/s: {item.volumeInfo.authors}</ListItem.Subtitle>
+                        <ListItem.Subtitle>Published year: {item.volumeInfo.publishedDate}</ListItem.Subtitle>
+                        <ListItem.Subtitle>Pages: {item.volumeInfo.pageCount}</ListItem.Subtitle>
                     </View>
                 </View>
             </ListItem.Content>
