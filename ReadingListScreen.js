3e2dbf049a82b02cf299a3d1bc60964594c971da
UllaMontonen <u.montonen@gmail.com>
2023-11-06 14:50:14 +0200

    ReadingListScreen created

---
+++ ReadingListScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+
+export default function ReadingListScreen() {
+    return (
+        <View>
+            <Text>ReadingList</Text>
+        </View>
+    )
+}
